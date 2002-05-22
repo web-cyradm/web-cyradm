825a2237d95b0460f50628b814073bb0ea672284
jeffbert <jeffbert@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2002-05-22 19:29:33 +0000

    added delete domainadmin code

--- deletedomain.php
+++ deletedomain.php
@@ -10,14 +10,14 @@ if ($admintype==0){
 	$query1="SELECT * FROM accountuser WHERE domain_name='$domain' order by username";
 	$result1=mysql_db_query($MYSQL_DB,$query1,$handle);
 	$cnt1=mysql_num_rows($result1);
-
+        
 
 	if (!$confirmed){
 
 		?>
 		<h3>Delete a Domain from the System</h3>
 
-		<h3>Do you really want to delete the Domain <?php print $domain ?> with all its defined accounts and emailadresses?</h3>
+		<h3>Do you really want to delete the Domain <font color=red><?php print $domain ?></font> with all its defined accounts, admins, and emailadresses?</h3>
 		This can take a while depending on how many account have to be deleted<p>
 
 		<font color="red">Your action will delete <?php print $cnt1 ?> accounts </font><p>
@@ -61,11 +61,25 @@ if ($admintype==0){
 			$query5="DELETE FROM virtual WHERE username='$username'";
 			$result5=mysql_db_query($MYSQL_DB,$query5);
 
+                        if ($DOMAIN_AS_PREFIX){
+				print $cyr_conn -> deletemb("user/".$username);
+			}
+			else {
+				print $cyr_conn -> deletemb("user.".$username);
+			}
 
-			print $cyr_conn->setacl('user.".$username',"$CYRUS_USERNAME","lrswipcda");
-			print $cyr_conn -> deletemb("user.".$username);
-
-	}
+		}
+	
+		$query6="SELECT * FROM domainadmin WHERE domain_name='$domain'";
+		$result6=mysql_db_query($MYSQL_DB,$query6);
+		$cnt6=mysql_num_rows($result6);
+                for ($i=0;$i<$cnt6;$i++){
+			$username=mysql_result($result6,$i,"adminuser");
+                        $query7="DELETE FROM adminuser where username='$username'";
+                        $result7=mysql_db_query($MYSQL_DB,$query7);
+                } 
+		$query8="DELETE FROM domainadmin WHERE domain_name='$domain'";
+                $hnd8=mysql_db_query($MYSQL_DB,$query8); 
 	print "<h3>Domain ".$domain." sucessfully deleted</h3>";
 
 	include ("browse.php");
