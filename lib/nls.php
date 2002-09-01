e80778cc02bc13ffb8d209c1e8e18c8a11372bac
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2002-09-01 21:03:12 +0000

    Added NLS file for getting valid languages

---
+++ lib/nls.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ ** Aliases for languages with different browser and gettext codes
+ **/
+
+
+//$nls['aliases']['bg'] = 'bg_BG';
+//$nls['aliases']['cs'] = 'cs_CZ';
+//$nls['aliases']['da'] = 'da_DK';
+$nls['aliases']['de'] = 'de_DE';
+//$nls['aliases']['el'] = 'el_GR';
+//$nls['aliases']['en'] = 'en_US';
+$nls['aliases']['es'] = 'es_ES';
+//$nls['aliases']['et'] = 'et_EE';
+//$nls['aliases']['fi'] = 'fi_FI';
+//$nls['aliases']['fr'] = 'fr_FR';
+$nls['aliases']['hu'] = 'hu_HU';
+//$nls['aliases']['it'] = 'it_IT';
+//$nls['aliases']['ja'] = 'ja_JP';
+//$nls['aliases']['ko'] = 'ko_KR';
+//$nls['aliases']['lt'] = 'lt_LT';
+//$nls['aliases']['nl'] = 'nl_NL';
+//$nls['aliases']['nn'] = 'nn_NO';
+//$nls['aliases']['no'] = 'nb_NO';
+//$nls['aliases']['pl'] = 'pl_PL';
+$nls['aliases']['pt'] = 'pt_PT';
+//$nls['aliases']['ru'] = 'ru_RU';
+//$nls['aliases']['sk'] = 'sk_SK';
+//$nls['aliases']['sl'] = 'sl_SI';
+//$nls['aliases']['sv'] = 'sv_SE';
+//$nls['aliases']['uk'] = 'uk_UA';
+$nsl['aliases']['zh'] = 'zh_CN';
+
+
+/**
+ ** Charsets
+ **/
+
+$nls['charsets']['bg_BG'] = 'windows-1251';
+$nls['charsets']['cs_CZ'] = 'ISO-8859-2';
+$nls['charsets']['el_GR'] = 'ISO-8859-7';
+$nls['charsets']['et_EE'] = 'ISO-8859-13';
+$nls['charsets']['hu_HU'] = 'ISO-8859-2';
+$nls['charsets']['ja_JP'] = 'SHIFT_JIS';
+$nls['charsets']['ko_KR'] = 'EUC-KR';
+$nls['charsets']['lt_LT'] = 'ISO-8859-13';
+$nls['charsets']['pl_PL'] = 'ISO-8859-2';
+$nls['charsets']['ru_RU'] = 'windows-1251';
+$nls['charsets']['ru_RU.KOI8-R'] = 'KOI8-R';
+$nls['charsets']['sk_SK'] = 'ISO-8859-2';
+$nls['charsets']['sl_SI'] = 'ISO-8859-2';
+$nls['charsets']['uk_UA'] = 'KOI8-U';
+$nls['charsets']['zh_CN'] = 'GB2312';
+$nls['charsets']['zh_TW'] = 'BIG5';
+
+
+
+?>
