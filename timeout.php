e3d6b19ffcd3a75a5ce861d763c9625d8b0bd760
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2002-10-22 20:19:10 +0000

    Changed URL in footers to www.web-cyradm.org

--- timeout.php
+++ timeout.php
@@ -72,7 +72,7 @@ function entsub() {
         <td height="20" valign="bottom" bgcolor="#CCCCCC">&nbsp;</td>
         <td height="20" valign="bottom" class="footer" bgcolor="#CCCCCC">
           &copy;  2002 by Luc de Louw | contact<a href="mailto:luc at delouw.ch"> luc at delouw.ch</a>| see
-<a href="http://www.delouw.ch/linux/web-cyradm/" target=_new>Web-cyradm</a>     </td>
+<a href="http://www.web-cyradm.org" target=_new>Web-cyradm</a>     </td>
   </tr>
 </table>
 
