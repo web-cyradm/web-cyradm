7032956f192aa33c7603e93da3cd531d96f87faf
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2003-04-12 14:40:54 +0000

    Allow more that one adress as destination (Alexander Skwar)

--- forwardaccount.php
+++ forwardaccount.php
@@ -17,21 +17,34 @@ if ($authorized){
 		die (_("Database error"));
 	}
 
-    if($setforward==1 && !empty($forwardto)) {
+    if($_POST['setforward'] == "1" && !empty($_POST['forwardto'])) {
         // delete all first
-        $query="delete from virtual WHERE alias='$username'";
+        $query = "delete from virtual WHERE alias=" . $handle->quote($_GET['username']);
         $result=$handle->query($query);
-        // insert new forward
-        $query="insert into virtual (alias,dest) VALUES('$username','$forwardto')";
-        $result=$handle->query($query);
+	$forwards = explode("\n", $_POST['forwardto']);
+	reset ($forwards);
+	$query = "insert into virtual (alias,dest) VALUES (" . $handle->quote($_GET['username'])  . ", ";
+	$q = array();
+	while (list(, $forward) = each($forwards)) {
+		// insert new forwards
+		$q[] = substr(trim($forward), 0, 255);
+	}
+	if (isset($_POST['metoo'])){
+		$keep = ' checked ';
+		$q[] = $username;
+	} else {
+		$keep = '';
+	}
+	$query .= $handle->quote(implode(',', $q)) . ')';
+	$result=$handle->query($query);
         $msg=_("Forward set");
-    } elseif($setforward==2) {
-        $query="delete from virtual WHERE alias='$username'";
+    } elseif($_POST['setforward'] == "2") {
+        $query="delete from virtual WHERE alias=" . $handle->quote($_GET['username']);
         $result=$handle->query($query);
         $msg=_("Forwarding removed");
     }
 
-    $query="select * from virtual where alias='$username'";
+    $query="select * from virtual where alias=" . $handle->quote($_GET['username']);
     $result=$handle->query($query);
     $row=$result->fetchRow(DB_FETCHMODE_ASSOC, 0);
     if(is_array($row))
@@ -43,13 +56,40 @@ if ($authorized){
     }
 ?>
 <h3><?php print _("Forward for account")." <font color=\"red\">".$username;?></font></h3> 
-<form action="<?php echo $PHP_SELF."?domain=$domain&username=$username&alias=$alias&action=$action";?>" method="POST">
+<form action="<?php 
+printf ('%s?domain=%s&amp;username=%s&amp;alias=%s&amp;action=%s', 
+	$_SERVER['PHP_SELF'],
+        htmlspecialchars($_GET['domain']),
+	htmlspecialchars($_GET['username']),
+	htmlspecialchars($_GET['alias']),
+	htmlspecialchars($_GET['action'])
+);
+?>" method="POST">
 <table>
 <tr>
-<td><input type="radio" name="setforward" value="1"<?php echo ($forw_is_set)?' checked':'';?>><?php echo _("Set forwarding to");?>:</td>
-<td><input class="inputfield" type="text" name="forwardto" value="<?php echo ($forw_is_set)?$row['dest']:'';?>" size="30"></td>
+<td><input type="radio" name="setforward" value="1"<?php echo ($forw_is_set)?' checked':'';?>><?php echo _("Set forwarding to (one adress per line)");?>:</td>
 </tr>
-<td colspan="2"><input type="radio" name="setforward" value="2"<?php echo (!$forw_is_set)?' checked':'';?>><?php echo _("Remove forwarding");?></td>
+<tr>
+<td><textarea name="forwardto" class="inputfield" cols="30" rows="5"><?php
+	if ($forw_is_set){
+		$forwards_tmp = preg_split('|,\s*|', stripslashes($row['dest']));
+		$forwards = array();
+		while (list(, $forward) = each($forwards_tmp)){
+			// If a mail is to be kept on the server,
+			// the $row[dest] also contains the $username
+			// filter it out
+			if (strtolower($forward) != strtolower($username)){
+				$forwards[] = htmlspecialchars(trim($forward));
+			}
+		}
+		echo implode("\n", $forwards);
+	}
+?></textarea><br>
+<input type="checkbox" name="metoo" <?php print $keep ."> " . _("Keep a copy in the user's mailbox");?><br><br>
+</td>
+</tr>
+<tr>
+<td><input type="radio" name="setforward" value="2"<?php echo (!$forw_is_set)?' checked':'';?>><?php echo _("Remove forwarding");?></td>
 </tr>
 </table>
 <input class="button" type="submit" value="<?php echo _("Submit");?>">
