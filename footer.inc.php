a3c12a94d51bf6786805f7fe94c8b265713386cf
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2003-05-07 19:38:54 +0000

    Changed copyright notice

--- footer.inc.php
+++ footer.inc.php
@@ -5,10 +5,10 @@
 				<td height="5%" valign="bottom" bgcolor="#CCCCCC">&nbsp;</td>
 				
 				<td height="5%" valign="bottom" class="footer" bgcolor="#CCCCCC">
-					&copy; 2002 by Luc de Louw | contact<a 
+					&copy; 2002, 2003 by Luc de Louw | contact<a 
 					href="mailto:luc at delouw.ch"> luc at delouw.ch</a>|
 					see <a href="http://www.web-cyradm.org"
-					target="_new">Web-cyradm</a>
+					target="_new">http://www.web-cyradm.org</a>
 				</td>
 			</tr>
 		</table>
