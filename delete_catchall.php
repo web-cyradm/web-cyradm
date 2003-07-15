1558b919a984c4e1c9a2b94e8fe72c31b910674f
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2003-07-15 23:41:46 +0000

    Adding delete_catchall

---
+++ delete_catchall.php
@@ -0,0 +1,46 @@
+<!-- #################### delete_catchall.php start #################### -->
+<tr>
+	<td width="10">&nbsp;</td>
+	<td valign="top">
+
+		<?php
+		if (! empty($confirmed) AND empty($cancel)){
+
+			# First Delete the entry from the database
+
+			$deletequery = "DELETE from virtual WHERE alias='@$domain'";
+
+			# And then add the new one
+
+			$handle=DB::connect($DB['DSN'], true);
+			if (DB::isError($handle)) {
+				die (_("Database error"));
+			}
+
+			$result = $handle->query($deletequery);
+
+			if ($result){
+				?>
+				<h3>
+					<?php print _("successfully deleted from the Database");?>
+				</h3>
+				<?php
+			} else {
+				?>
+				<h3>
+					<?php print _("Database error, please try again");?>
+				</h3>
+				<?php
+			}
+		} elseif (! empty($cancel)){
+			?>
+			<h3>
+				<?php print _("Cancelled");?>
+			</h3>
+			<?php
+		}
+		?>
+	</td>
+</tr>
+<!-- #################### delete_catchall.php end #################### -->
+
