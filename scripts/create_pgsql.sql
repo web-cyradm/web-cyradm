052ec3e1ed5a2b16c4e6d02278d86a13f7b59899
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2003-07-16 23:34:43 +0000

    Merge from 0.5.4 branch

--- scripts/create_pgsql.sql
+++ scripts/create_pgsql.sql
@@ -1,34 +1,50 @@
+DROP INDEX virtual_username_ndx;
+DROP INDEX virtual_unique_ndx;
+DROP INDEX domain_unique_ndx;
+DROP INDEX accountuser_unique_ndx;
+DROP TABLE search;
+DROP TABLE virtual;
+DROP TABLE domainadmin;
+DROP TABLE "domain";
+DROP TABLE "log";
+DROP TABLE alias;
+DROP TABLE adminuser;
+DROP TABLE accountuser;
+
 CREATE TABLE accountuser (
-  username varchar(30) NOT NULL UNIQUE default '',
-  password varchar(30) NOT NULL default '',
-  prefix varchar(30) NOT NULL default '',
+  username varchar(255) NOT NULL default '',
+  password varchar(50) NOT NULL default '',
+  prefix varchar(50) NOT NULL default '',
   domain_name varchar(255) NOT NULL default ''
 );
 
+CREATE UNIQUE INDEX accountuser_ndx ON accountuser(username);
+
 CREATE TABLE adminuser (
-  username varchar(30) NOT NULL default '',
-  password varchar(30) NOT NULL default '',
-  type int NOT NULL default '0',
+  username varchar(50) PRIMARY KEY,
+  password varchar(50) NOT NULL default '',
+  type int NOT NULL default 0,
   SID varchar(255) NOT NULL default '',
-  home varchar(255) NOT NULL default '',
-  PRIMARY KEY (username)
+  home varchar(255) NOT NULL default ''
 );
 
 CREATE TABLE alias (
-  alias varchar(255) NOT NULL default '',
-  dest text,
-  username varchar(30) NOT NULL default '',
-  status int NOT NULL default '1',
-  PRIMARY KEY (alias)
+  alias varchar(255) PRIMARY KEY,
+  dest varchar(255),
+  username varchar(255) NOT NULL default '',
+  status int NOT NULL default 1
 );
 
 CREATE TABLE domain (
-  domain_name varchar(255) NOT NULL default '',
-  prefix varchar(30) NOT NULL UNIQUE default '',
-  maxaccounts int NOT NULL default '20',
+  domain_name varchar(255) PRIMARY KEY ,
+  prefix varchar(255) UNIQUE NOT NULL default '',
+  maxaccounts int NOT NULL default 20,
   quota int NOT NULL default '20000',
-  PRIMARY KEY (domain_name)
+  freenames varchar(3) CHECK (freenames='YES' OR freenames='NO' ) DEFAULT 'NO' NOT NULL,
+  freeaddress varchar(3) CHECK (freeaddress='YES' OR freeaddress='NO' ) DEFAULT 'NO' NOT NULL,
+  transport varchar(255) NOT NULL default 'cyrus'
 );
+
 
 CREATE TABLE domainadmin (
   domain_name varchar(255) NOT NULL default '',
@@ -36,20 +52,27 @@ CREATE TABLE domainadmin (
 );
 
 CREATE TABLE search (
-  search_id varchar(255) NOT NULL default '',
+  search_id varchar(255) PRIMARY KEY,
   search_sql text NOT NULL,
   perpage int NOT NULL default '0',
-  timestamp timestamp(13) NOT NULL,
-  PRIMARY KEY (search_id)
+  timestamp timestamp NOT NULL
 );
-CREATE INDEX search_search_id_key ON search (search_id);
 
 CREATE TABLE virtual (
-  alias varchar(255) NOT NULL UNIQUE default '',
-  dest text,
-  username varchar(30) NOT NULL default '',
-  status int NOT NULL default '1',
-  PRIMARY KEY (alias)
+  alias varchar(255) PRIMARY KEY ,
+  dest varchar(255),
+  username varchar(255) NOT NULL default '',
+  status int NOT NULL default '1'
+);
+CREATE UNIQUE INDEX virtual_unique_ndx ON virtual(alias,dest);
+
+CREATE TABLE log (
+  id serial PRIMARY KEY,
+  msg text NOT NULL,
+  "user" varchar(255) NOT NULL default '',
+  host varchar(255) NOT NULL default '',
+  time timestamp  NOT NULL,
+  pid varchar(255) NOT NULL default ''
 );
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON accountuser TO mail;
@@ -59,3 +82,4 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON domain TO mail;
 GRANT SELECT,INSERT,UPDATE,DELETE ON domainadmin TO mail;
 GRANT SELECT,INSERT,UPDATE,DELETE ON search TO mail;
 GRANT SELECT,INSERT,UPDATE,DELETE ON virtual TO mail;
+GRANT SELECT,INSERT,UPDATE,DELETE ON log TO mail;
