f35e9f0f68096e49e9124382e61ea325d5c7cdb6
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2003-09-14 14:11:25 +0000

    Alias not UNIQUE anymore

--- scripts/create_mysql.sql
+++ scripts/create_mysql.sql
@@ -100,8 +100,7 @@ CREATE TABLE virtual (
   dest longtext,
   username varchar(50) NOT NULL default '',
   status int(11) NOT NULL default '1',
-  PRIMARY KEY  (alias),
-  UNIQUE KEY alias (alias)
+  KEY alias (alias)
 ) TYPE=MyISAM;
 
 CREATE TABLE log (
