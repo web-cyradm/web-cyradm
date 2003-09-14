5c2f8cf27d16de718310c9de449c05b0fd97be79
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2003-09-14 13:32:33 +0000

    Added freenames

--- scripts/create_mysql.sql
+++ scripts/create_mysql.sql
@@ -60,7 +60,8 @@ CREATE TABLE domain (
   maxaccounts int(11) NOT NULL default '20',
   quota int(10) NOT NULL default '20000',
   transport varchar(255) NOT NULL default 'cyrus',
-  freeaddress ENUM( 'YES', 'NO' ) DEFAULT 'NO' NOT NULL,
+  freenames ENUM( 'YES', 'NO' ) DEFAULT 'NO' NOT NULL,
+  freeadress ENUM( 'YES', 'NO' ) DEFAULT 'NO' NOT NULL,
   PRIMARY KEY  (domain_name),
   UNIQUE KEY prefix (prefix)
 ) TYPE=MyISAM;
