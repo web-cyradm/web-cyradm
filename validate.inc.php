b79f3081a789115fd6f232ffeccbffe767430f29
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2004-08-25 21:47:28 +0000

    Fixed race condition: adminuser gets superuser when deleted while in a session

--- validate.inc.php
+++ validate.inc.php
@@ -67,6 +67,13 @@ if (DB::isError($handle)) {
 
 $result = $handle->query($query);
 $result2 = $handle->query($query2);
+
+if (!$cnt){
+        print _("Security violation detected, attempt logged");
+        include WC_BASE . "/logout.php";
+        die ();
+}
+
 $cnt = $result->numRows();
 $row = $result2->fetchRow(DB_FETCHMODE_ASSOC, 0);
 $admintype = $row['type'];
