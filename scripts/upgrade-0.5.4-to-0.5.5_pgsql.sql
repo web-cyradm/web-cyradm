7b1280aaacdd7ceaa759dd277f95304fa8dc223b
lukasz <lukasz@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2004-11-28 23:02:08 +0000

    Corrected domainquota definition

--- scripts/upgrade-0.5.4-to-0.5.5_pgsql.sql
+++ scripts/upgrade-0.5.4-to-0.5.5_pgsql.sql
@@ -1,6 +1,6 @@
 -- From PostgreSQL documentation: "In the current implementation of ADD COLUMN, default and NOT NULL clauses for the new column are not supported. The new column always comes into being with all values null."
 -- How to achive "AFTER quota"?
-ALTER TABLE domain ADD domainquota int(10);
+ALTER TABLE domain ADD domainquota int;
 ALTER TABLE domain SET DEFAULT '0';
 UPDATE domain SET domainquota = DEFAULT;
 ALTER TABLE domain ALTER COLUMN domainquota SET NOT NULL;
