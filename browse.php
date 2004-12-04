5e17401cb5980cedea735dca67e73ead57f0eccf
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2004-12-04 22:53:19 +0000

    Introduced limited view of domains by _SESSION['maxdisplay']

--- browse.php
+++ browse.php
@@ -14,41 +14,30 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 		<h3>
 			<?php print _("Browse domains");?>
 		</h3>
+		<?php
+		$row_pos = (empty($row_pos))?(0):($row_pos);
+		if (! isset($_SESSION['allowed_domains'])) {
+			$query2 = "SELECT * FROM domain ORDER BY domain_name";
+		} 
+		else {
+			$allowed_domains = '';
+			foreach ($_SESSION['allowed_domains'] as $allowed_domain) {
+			$allowed_domains .= $allowed_domain."' OR domain_name='";
+		 }
+		$query2 = "SELECT * FROM domain WHERE domain_name='$allowed_domains' ORDER BY domain_name";
+		}
 
-		<table border="0" width="98%">
-			<tbody>
-				<tr>
-					<th colspan="4">
-						<?php print _("action");?>
-					</th>
+	        $result2 = $handle->query($query2);
+        	$total=$result2->numRows($result2);
 
-					<th>
-						<?php print _("domainname");?>
-					</th>
+?>
 
-					<?php
-					if (! $DOMAIN_AS_PREFIX){
-						?>
-						<th>
-							<?php print _("prefix");?>
-						</th>
-						<?php
-					}
-					?>
 
-					<th>
-						<?php print _("max Accounts");?>
-					</th>
-					
-					<th>
-						<?php print _("max Domain quota");?>
-					</th>
+	
 
-					<th>
-						<?php print _("default quota per user");?>
-					</th>
-				</tr>
-				
+
+
+		<table border="1" width="98%">
 				<?php
 
 				if (! isset($_SESSION['allowed_domains'])) {
@@ -62,8 +51,80 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 //					print $query;
 				}
 
-				$result = $handle->query($query);
+				$result = $handle->limitQuery($query,$row_pos,$_SESSION['maxdisplay']);
 				$cnt    = $result->numRows($result);
+
+				print _("Total Domains")." ".$total;
+				print "<br>"._("Displaying from position:")." $row_pos";
+				
+				?>
+<!-- 		</table> -->
+                <table cellspacing="2" cellpadding="0">
+                        <tr>
+                                <td class="navi">
+                                        <a href="index.php?action=newdomain&domain=new"><?php print _("Add new domain");?></a>
+                                </td>
+
+                                <?php
+                                $prev = $row_pos - $_SESSION['maxdisplay'];
+                                $next = $row_pos + $_SESSION['maxdisplay'];
+
+                                if ($row_pos < $_SESSION['maxdisplay']){
+					print "<td class=\"navi\"><a href=\"#\">"._("Previous entries")."</a></td>";
+                                } else {
+					print "<td class=\"navi\"><a href=\"index.php?action=accounts&domain=$domain&row_pos=$prev\">"._("Previous entries") ."</a></td>";		
+                                }
+
+				if ($next>$total){
+					print "<td class=\"navi\"><a href=\"#\">"._("Next 10 entries")."</a></td>";
+				}
+				else {
+					print "<td class=\"navi\"><a href=\"index.php?action=accounts&domain=$domain&row_pos=$next\">". _("Next entries")."</a></td>";
+				}
+                                ?>
+
+
+                        </tr>
+                 </table> 
+		<table>
+
+
+                       <tbody>
+                                <tr>
+                                        <th colspan="4">
+                                                <?php print _("action");?>
+                                        </th>
+
+                                        <th>
+                                                <?php print _("domainname");?>
+                                        </th>
+
+                                        <?php
+                                        if (! $DOMAIN_AS_PREFIX){
+                                                ?>
+                                                <th>
+                                                        <?php print _("prefix");?>
+                                                </th>
+                                                <?php
+                                        }
+                                        ?>
+
+                                        <th>
+                                                <?php print _("max Accounts");?>
+                                        </th>
+
+                                        <th>
+                                                <?php print _("max Domain quota");?>
+                                        </th>
+
+                                        <th>
+                                                <?php print _("default quota per user");?>
+                                        </th>
+                                </tr>
+
+
+<?php
+				
 
 				for ($c=0; $c < $cnt; $c++){
 					if ($c%2==0){
