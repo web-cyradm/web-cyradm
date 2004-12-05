7d864081516a0f7d406b0fd07de19f084cb124fc
luc <luc@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2004-12-05 14:24:39 +0000

    fixed typo

--- validate.inc.php
+++ validate.inc.php
@@ -16,7 +16,7 @@ $_get_vars = array(
 	'newdomain', 'email', 'alias', 'dest', 'newalias', 'newdest', 'confirmed',
 	'cancel', 'searchstring', 'transport', 'tparam', 'mode', 'forwards',
 	'metoo', 'vacation_text', 'freenames', 'freeaddress', 'LANG', 'delete_catchall',
-	'resp_domain', 'newdomain','oderby'
+	'resp_domain', 'newdomain','orderby'
 );
 
 $_post_vars = array(
