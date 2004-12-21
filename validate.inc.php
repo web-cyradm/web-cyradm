d4a1c71a0d3c87a88a817dc383bd6239a8139743
lukasz <lukasz@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2004-12-21 16:07:49 +0000

    Add validation for all POST actions

--- validate.inc.php
+++ validate.inc.php
@@ -17,14 +17,10 @@ $_get_vars = array(
 	'resp_domain', 'newdomain','orderby'
 );
 
-$_post_vars = array(
-	'confirmed', 'action', 'domain', 'alias' , 'username', 'new_password',
-	'confirm_password', 'email', 'quota', 'password','newadminuser', 'newadmintype',
-	'newdomain', 'newtype', 'resp_domain');
+$_post_vars = array('action','domain');
 
 if ($_GET){
 	if ($_GET['action']!=""){
-
 		foreach ($_get_vars as $_get_var){
 			if (isset($_GET[$_get_var])){
 				$$_get_var = $_GET[$_get_var];
@@ -52,6 +48,14 @@ function ValidateMail($email) {
 		return 0;
 	} else {
 		return 1;
+	}
+}
+
+function ValidMail($email) {
+	if (!eregi("^[0-9a-z]([-_.]?[0-9a-z])*@[0-9a-z]([-.]?[0-9a-z])*\\.[a-wyz][a-z](g|l|m|pa|t|u|v)?$",$email)){
+		return FALSE;
+	} else {
+		return TRUE;
 	}
 }
 
@@ -85,15 +89,22 @@ function ValidPrefix($prefix) {
 	if ($DOMAIN_AS_PREFIX) {
 		return ValidDomain($prefix);
 	} else {
-		if (!eregi("^[[:alnum:]_-]+$",$prefix)){
+//		if (!eregi("^[[:alnum:]_-]+$",$prefix)){
+		if (empty($prefix)) {
 			return FALSE;
 		} else {
 			return TRUE;
 		}
 	}
-	
 }
 
+function ValidAdminType($type) {
+	if ($type == 0 || $type == 1) {
+		return TRUE;
+	} else {
+		return FALSE;
+	}
+}
 
 # Security precaution if register_globals = on
 $authorized = FALSE;
@@ -133,7 +144,7 @@ if ($_SESSION['admintype'] != 0){
 	$_SESSION['allowed_domains'] = $allowed_domains;
 	if (!isset($domain)) $domain='';
 
-	if (isset($domain) AND $domain != "" AND !in_array($domain,$_SESSION['allowed_domains'])) {
+	if (!empty($domain) AND !in_array($domain,$_SESSION['allowed_domains'])) {
 		print _("Security violation detected, attempt logged");
 		logger(sprintf("SECURITY VIOLATION %s %s %s %s %s%s", $_SERVER['REMOTE_ADDR'], $_SESSION['user'], $_SERVER['HTTP_USER_AGENT'], $_SERVER['HTTP_REFERER'], $_SERVER['REQUEST_METHOD'], "\n"),"WARN");
 		include WC_BASE . "/logout.php";
@@ -163,13 +174,229 @@ if (! empty($action)){
 			unset($_GET['row_pos']);
 		}
 		break;
-############################## Check deleteaccount ##################################################
+#OK########################### Check input if newadminuser ###############################################
+	case "newadminuser":
+		if ($_SESSION['admintype'] != 0) {
+			$authorized = FALSE;
+			$err_msg = _("Security violation detected, nothing deleted, attempt has been logged");
+		} elseif (!isset($_POST['confirmed'])) {
+			if (!empty($_GET['domain']) && !ValidDomain($_GET['domain'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} else {
+				$authorized = TRUE;
+			}
+		} else {
+			if (!empty($_POST['domain']) && !ValidDomain($_POST['domain']) || !ValidAdminType($_POST['newadmintype'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} elseif (!ValidName($_POST['newadminuser'])) {
+				$authorized = FALSE;
+				$err_msg = _("You must provide a username");
+			} elseif (!ValidPassword($_POST['new_password']) || !ValidPassword($_POST['confirm_password'])){
+				$authorized = FALSE;
+				$err_msg = _("Password incorrect");
+			} elseif ($_POST['new_password'] != $_POST['confirm_password']) {
+				$authorized = FALSE;
+				$err_msg = _("New passwords are not equal. Password not changed");
+			} else {
+				# Check if admin already exists
+				$query = "SELECT * FROM adminuser WHERE username='".$_POST['newadminuser']."'";
+				$result = $handle->query($query);
+				if (DB::isError($result)) {
+					die (_("Database error"));
+				}
+				if ($result->numRows()){
+					$authorized = FALSE;
+					$err_msg = _("Username already exist");
+				# If domain is not set or admin will be superuser: that's all
+				} elseif (empty($_POST['newdomain']) || $_POST['newadmintype'] == 0) {
+					$authorized = TRUE;
+				} elseif (!ValidDomain($_POST['newdomain'])) {
+					$authorized = FALSE;
+					$err_msg = _("Invalid domain name");
+				} else {
+					# Check if domain already exists
+					$query="SELECT domain_name FROM domain WHERE domain_name='".$_POST['newdomain']."'";
+					$result= $handle->query($query);
+					if (DB::isError($result)) {
+						die (_("Database error"));
+					}
+					if (!$result->numRows()){
+						$authorized = FALSE;
+						$err_msg = _("No such domain");
+					} else {
+						$authorized = TRUE;
+					}
+				}
+			}
+		}
+		break;
+#Almost OK################### Check input if editadminuser ###############################################
+	case "editadminuser":
+		if ($_SESSION['admintype'] != 0) {
+			$authorized = FALSE;
+			$err_msg = _("Security violation detected, nothing deleted, attempt has been logged");
+		} elseif (!isset($_POST['confirmed'])) {
+			if (!empty($_GET['domain']) && !ValidDomain($_GET['domain'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} elseif (!ValidName($_GET['username'])) {
+				$authorized = FALSE;
+				$err_msg = _("You must provide a username");
+			} else {
+				$authorized = TRUE;
+			}
+		} else {
+			if (!empty($_POST['domain']) && !ValidDomain($_POST['domain']) || !ValidAdminType($_POST['newtype'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} elseif (!ValidName($_POST['username'])) {
+				$authorized = FALSE;
+				$err_msg = _("You must provide a username");
+			} elseif (!empty($_POST['new_password']) && (!ValidPassword($_POST['new_password']) || !ValidPassword($_POST['confirm_password']))){
+				$authorized = FALSE;
+				$err_msg = _("Password incorrect");
+			} elseif ($_POST['new_password'] != $_POST['confirm_password']) {
+				$authorized = FALSE;
+				$err_msg = _("New passwords are not equal. Password not changed");
+			} else {
+				$query = "SELECT `type` from adminuser WHERE username='".$_POST['username']."'";
+				$result = $handle->query($query);
+				if (DB::isError($result)) {
+					die (_("Database error"));
+				}
+				$row = $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
+				$type = $row['type'];
+				
+				if ($type != $_POST['newtype']) {
+					if ($_POST['newtype'] == 1) {
+						# We have to take care to have at least one superuser left, or we cannot use
+						# Web-cyradm again
+
+						# Query to get the count of superusers
+						$query = "SELECT `type` FROM adminuser WHERE type='0'";
+						$result = $handle->query($query);
+						if (DB::isError($result)) {
+							die (_("Database error"));
+						}
+						$cnt = $result->numRows($result);
+
+						# Check if only 1 superuser is defined, in case of requested change of a superuser
+						if ($cnt==1 && $type==0){
+							# No Way! We cannot change the last Superuser to domainadmin!
+							$authorized = FALSE;
+							$err_msg = _("At least one Superuser is needed for Web-cyradm");
+						}
+					}
+				}
+				# Required checks for array of domains $_POST['resp_domain']
+				//if (!empty($_POST['resp_domain'])) {
+				//}
+				# If domain is not set: that's all
+				if (empty($_POST['newdomain']) || $_POST['newtype'] == 0) {
+					$authorized = TRUE;
+				} elseif (!ValidDomain($_POST['newdomain'])) {
+					$authorized = FALSE;
+					$err_msg = _("Invalid domain name");
+				} else {
+					# Check if domain already exists
+					$query = "SELECT domain_name FROM domain WHERE domain_name='".$_POST['newdomain']."'";
+					$result = $handle->query($query);
+					if (DB::isError($result)) {
+						die (_("Database error"));
+					}
+					if (!$result->numRows()){
+						$authorized = FALSE;
+						$err_msg = _("No such domain");
+					} else {
+						$query = "SELECT * FROM domainadmin WHERE adminuser='".$_POST['username']."' AND domain_name='".$_POST['newdomain']."'";
+						$result = $handle->query($query);
+						if (DB::isError($result)) {
+							die (_("Database error"));
+						}
+						if ($result->numRows()) {
+							$err_msg = _("Admin already repsonsible for the domain")." ".$_POST['newdomain'];
+						} else {
+							$authorized = TRUE;
+						}
+					}
+				}
+			}
+		}
+		break;
+#OK############################# Check input if newaccount ################################################
+	case "newaccount":
+		if (!isset($_POST['confirmed'])) {
+			if (!empty($_GET['domain']) && !ValidDomain($_GET['domain'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} else {
+				$authorized = TRUE;
+			}
+		} else {
+			if (!ValidDomain($_POST['domain']) || !is_numeric($_POST['quota'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} elseif (!ValidPassword($_POST['password'] || !ValidPassword($_POST['confirm_password']))){
+				$authorized = FALSE;
+				$err_msg = _("Password incorrect");
+			} elseif ($_POST['password'] != $_POST['confirm_password']) {
+				$authorized = FALSE;
+				$err_msg = _("Passwords do not match");
+			} elseif (!ValidMail($_POST['email']."@".$_POST['domain'])) {
+				$authorized = FALSE;
+				$err_msg = _("Invalid email adress");
+			} elseif (!empty($_POST['username']) && !ValidName($_POST['username'])) {
+				$authorized = FALSE;
+				$err_msg = _("Invalid username");
+			# Check for reserved addresses
+			} elseif (in_array($_POST['email'], $reserved)) {
+				$authorized = FALSE;
+				$err_msg="Reserved Emailadress, request cancelled";
+			} else {
+				$authorized = TRUE;
+				
+				if ($DOMAIN_AS_PREFIX){
+					$_POST['username'] = $_POST['email'];
+				}
+				// check to see if there's an account with the same username
+				$query = "SELECT * FROM accountuser WHERE username='".$_POST['username']."'";
+				$result = $handle->query($query);
+				if (DB::isError($result)) {
+					die (_("Database error"));
+				}
+				$cnt = $result->numRows();
+				if ($cnt != 0) {
+					$authorized = FALSE;
+					$err_msg = _("Sorry, the username already exists");
+				}
+				
+				# We need to check if the requested quota is NOT higher than the defined maximum Quota
+				# Superusers can override
+				$query = "SELECT quota FROM domain WHERE domain_name='".$_POST['domain']."'";
+				$result = $handle->query($query);
+				$row = $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
+				# $quota2 is the allowed quota, $quota the requested quota for the account
+				$quota2=$row['quota'];
+
+				#When the requuested quota is higher that the default quota, we need to check if
+				#admin NOT superuser AND when submitting the request
+				if ($_POST['quota'] > $row['quota'] && $_SESSION['admintype'] != 0) {
+					$authorized = FALSE;
+					$err_msg = _("Quota exeedes the maximum allowed quota for this domain.");
+				}
+			}
+		}
+		break;
+#OK########################### Check input if deleteaccount ###############################################
 	case "deleteaccount":
 		if (!ValidDomain($_GET['domain'])){
 			$authorized = FALSE;
 		} elseif (!ValidName($_GET['username'])) {
 			$authorized = FALSE;
 		} else {
+			# it's needed to defend 'cyrus' user
 			$query = "SELECT username FROM accountuser WHERE username='".$_GET['username']."' AND domain_name='".$_GET['domain']."'";
 			$result = $handle->query($query);
 			if (DB::isError($result)) {
@@ -182,39 +409,6 @@ if (! empty($action)){
 			}
 		}
 		break;
-
-################################ Check input if newaccount ################################################
-
-	case "newaccount":
-		# We need to check if the requested quota is NOT higher than the defined maximum Quota
-		# Superusers can override
-		$query = "SELECT quota FROM domain WHERE domain_name='$domain'";
-		$result=$handle->query($query);
-		$row=$result->fetchRow(DB_FETCHMODE_ASSOC, 0);
-		# $quota2 is the allowed quota, $quota the requested quota for the account
-		$quota2=$row['quota'];
-
-		#When the requuested quota is higher that the default quota, we need to check if
-		#admin NOT superuser AND when submitting the request
-		if ($quota>$quota2 && $_SESSION['admintype']!=0 && $confirmed==TRUE){
-			$err_msg=_("Quota exeedes the maximum allowed qutoa for this domain.");
-			$authorized = FALSE;
-		}
-		else {
-			# If the requirements are not matches, deny submission
-			$authorized=TRUE;
-		}
-		# Check for reserved addresses
-		if (in_array($email, $reserved)) {
-			$authorized = FALSE;	
-			$err_msg="Reserved Emailadress, request cancelled";
-		}
-
-	break;
-
-
-
-
 ################################ Check input if setquota ##################################################
 	case "setquota":
 		$query = "SELECT quota FROM domain WHERE domain_name='$domain'";
@@ -239,11 +433,8 @@ if (! empty($action)){
 			die ();
 		}
 		break;
-
 ################################## Check input if newemail ################################################
 	case "newemail":
-		
-
 		$query = "SELECT * FROM accountuser WHERE username='$username' AND domain_name='$domain'";
 		$result = $handle->query($query);
 		$row = $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
@@ -268,7 +459,7 @@ if (! empty($action)){
 			} elseif (!$valid_alias and isset($alias)){
 //	        	elseif (!ValidateMail($alias."@".$domain) and isset($alias))
 				$authorized = FALSE;
-				$err_msg = "Invalid email adress";
+				$err_msg = _("Invalid email adress");
 			} else {
 				$authorized=TRUE;
 			}
@@ -282,17 +473,37 @@ if (! empty($action)){
 			$authorized = TRUE;
 		}
 		break;
-
-#####################  Check if change email-adress ####################################
-
+#OK############################ Check input if change_password ###############################
+	case "change_password":
+		if (!isset($_POST['confirmed'])) {
+			if (!ValidDomain($_GET['domain']) || !ValidName($_GET['username'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} else {
+				$authorized = TRUE;
+			}
+		} else {
+			if (!ValidDomain($_POST['domain']) || !ValidName($_POST['username'])) {
+				$authorized = FALSE;
+				$err_msg = "";
+			} elseif (!ValidPassword($_POST['new_password']) || !ValidPassword($_POST['confirm_password'])){
+				$authorized = FALSE;
+				$err_msg = _("Password incorrect");
+			} elseif ($_POST['new_password'] != $_POST['confirm_password']) {
+				$authorized = FALSE;
+				$err_msg = _("New passwords are not equal. Password not changed");
+			} else {
+				$authorized = TRUE;
+			}
+		}
+		break;
+###########################  Check if change email-adress ####################################
+	## FIXME: make beter checks
 	case "editemail":
 
-	## FIXME: make beter checks
-	case "change_password":
 	case "vacation":
 	case "forwardaccount":
 	case "forwardalias":
-
 		$query = "SELECT * FROM accountuser WHERE username='$username' AND domain_name='$domain'";
 		$result = $handle->query($query);
 
@@ -327,7 +538,6 @@ if (! empty($action)){
 		}
 
 		break;
-
 ######################################## Check new domain name ########################################
 	case "newdomain":
 		if (! empty($confirmed)){
@@ -371,7 +581,7 @@ if (! empty($action)){
 			}
 		}
 		break;
-##################################### Check input if changeadminpasswd ###############################
+#OK################################## Check input if changeadminpasswd ###############################
 	case "changeadminpasswd":
 		if (isset($_POST['confirmed'])){
 			if (!ValidPassword($_POST['old_password']) || !ValidPassword($_POST['new_password']) || !ValidPassword($_POST['confirm_password'])) {
@@ -429,25 +639,22 @@ if (! empty($action)){
 			$err_msg="Reserved Emailadress, request cancelled";
 		}
 		break;		
-		
+##########################################
 	case "deletealias":
 	case "forwardalias":
 	case "deleteemail":
 	case "vacation":
+		$query3 = "SELECT domain_name FROM accountuser WHERE username='$username' AND domain_name='$domain'";
+		$result3 = $handle->query($query3);
+		$cnt3=$result3->numRows();
 
-	$query3 = "SELECT domain_name FROM accountuser WHERE username='$username' AND domain_name='$domain'";
-	$result3 = $handle->query($query3);
-	$cnt3=$result3->numRows();
-
-	if (!$cnt3 and $username !=""){
-		print _("Security violation detected, attempt logged");
-		include WC_BASE . "/logout.php";
-		die ();
-	}
-
-
+		if (!$cnt3 and $username !=""){
+			print _("Security violation detected, attempt logged");
+			include WC_BASE . "/logout.php";
+			die ();
+		}
+		break;
 ######################################### If nothing matches ##########################################
-
 	default:
 		break;
 	} // End of switch ($action)
