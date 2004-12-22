184e0bd9581fa3e9a25e8c066076a27acdbf17ed
lukasz <lukasz@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2004-12-22 08:36:16 +0000

    Added LOG_LEVEL variable

--- lib/log.php
+++ lib/log.php
@@ -1,16 +1,27 @@
 <?
 function logger($entry, $level = 'INFO') {
 	global $LOG_DIR;
-	$fp = @fopen($LOG_DIR . "web-cyradm-login.log", "a");
-	
-	if ($fp) {
-		$date = date("M d H:i:s");
-		fwrite($fp, sprintf("%s [%s] %s%s", $date, $level, $entry, "\n"));
-		fclose($fp);
-		return TRUE;
-	}
-	else {
-		return FALSE;
+	global $LOG_LEVEL;
+
+	$levels = array (
+		'DEBUG'=> 0,
+		'INFO' => 1,
+		'WARN' => 2,
+		'ERR'=> 3
+	);
+
+	if ($levels[$level] >= $levels[$LOG_LEVEL]) {
+		$fp = @fopen($LOG_DIR . "web-cyradm-login.log", "a");
+
+		if ($fp) {
+			$date = date("M d H:i:s");
+			fwrite($fp, sprintf("%s [%s] %s%s", $date, $level, $entry, "\n"));
+			fclose($fp);
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
 	}
 }
 ?>
