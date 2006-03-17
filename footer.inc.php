a88810085c73e7b72082de0f61c410b640ecfa8a
lukasz <lukasz@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2006-03-17 10:31:20 +0000

    Updated copyright notice

--- footer.inc.php
+++ footer.inc.php
@@ -15,7 +15,7 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 				<td height="5" valign="bottom" class="footer" bgcolor="#CCCCCC">&nbsp;</td>
 				
 				<td height="5" valign="bottom" class="footer" bgcolor="#CCCCCC">
-					&copy; 2002-2005 by Luc de Louw | 
+					&copy; 2002-2006 by Luc de Louw | 
 					 <a href="http://www.web-cyradm.org"
 					target="_new">http://www.web-cyradm.org</a> | <?php print _("translated by: ").$nls['translator']["$LANG"]; ?> | you are running web-cyradm Version <?php print $VERSION; ?>
 				</td>
