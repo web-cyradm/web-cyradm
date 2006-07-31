86a952cd5e0d81ebf4cedb9c138b2cbdd2655d3f
lukasz <lukasz@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2006-07-31 19:11:56 +0000

    Code cleanup. Added validation. Moved from  to

--- forwardaccount.php
+++ forwardaccount.php
@@ -8,79 +8,67 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 ?>
 <!-- #################### forwardaccount.php start #################### -->
 <tr>
-	<td width="10">
-		<?php
-		if (! empty($err_msg)){
-			?>
-			<span style="color: red;">
-				<?php echo $err_msg;?>
-			</span>
-			<?php
-		} else {
-			echo "&nbsp;";
-		}
-		?>
-	</td>
+	<td width="10">&nbsp;</td>
 	<td valign="top">
-
-		<?php
-		if ($authorized){
-			$handle = DB::connect($DB['DSN'], true);
-			if (DB::isError($handle)) {
-				die (_("Database error"));
-			}
-
-			if (! empty($_POST['forwardto']) && $_POST['setforward'] == "1"){
-				// delete all first
-				$query = "DELETE FROM virtual WHERE alias=" . $handle->quote($_GET['username']);
-				$result = $handle->query($query);
-				$forwards = explode("\n", $_POST['forwardto']);
-				reset ($forwards);
-				$query = "INSERT INTO virtual (alias,dest) VALUES (" . $handle->quote($_GET['username'])  . ", ";
-				$q = array();
-				while (list(, $forward) = each($forwards)) {
-					// insert new forwards
-					$q[] = substr(trim($forward), 0, 255);
-				}
-				if (isset($_POST['metoo'])){
-					$keep = ' checked ';
-					$q[] = $username;
-				} else {
-					$keep = '';
-				}
-				$query .= $handle->quote(implode(',', $q)) . ')';
-				$result = $handle->query($query);
-				$msg = _("Forward set");
-			} elseif (! empty($_POST['setforward']) && $_POST['setforward'] == "2") {
-				$query = "DELETE FROM virtual WHERE alias=" . $handle->quote($_GET['username']);
-				$result = $handle->query($query);
-				$msg = _("Forwarding removed");
-			}
-
-			$query = "SELECT * FROM virtual WHERE alias=" . $handle->quote($_GET['username']);
+<?php
+if ($authorized){
+	if (!empty($_GET['confirmed'])) {
+		if (!empty($_GET['setforward']) && $_GET['setforward'] == "1") {
+			// delete all first
+			$query = "DELETE FROM virtual WHERE alias='".$_GET['username']."'";
 			$result = $handle->query($query);
-			$row = $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
-			if (is_array($row)){
-				$forw_is_set=1;
-			} else {
-				$forw_is_set=0;
+			$forwards = explode("\n", $_GET['forwardto']);
+			reset($forwards);
+			$query = "INSERT INTO virtual (alias,dest) VALUES ('".$_GET['username']."', '";
+			$q = array();
+			while (list(, $forward) = each($forwards)) {
+				// insert new forwards
+				$q[] = substr(trim($forward), 0, 255);
 			}
-			if (! empty($msg)){
-				?>
+			if (!empty($_GET['metoo'])){
+				$keep = ' checked ';
+				$q[] = $username;
+			} else {
+				$keep = '';
+			}
+			$query .= implode(',', $q)."')";
+			$result = $handle->query($query);
+			$msg = _("Forward set");
+		} elseif (!empty($_GET['setforward']) && $_GET['setforward'] == "2") {
+			$query = "DELETE FROM virtual WHERE alias='".$_GET['username']."'";
+			$result = $handle->query($query);
+			$msg = _("Forwarding removed");
+		}
+	}
+
+	$query = "SELECT * FROM virtual WHERE alias='".$_GET['username']."'";
+	$result = $handle->query($query);
+	$cnt = $result->numRows();
+	if ($cnt) {
+		$row = $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
+		$forw_is_set = 1;
+	} else {
+		$forw_is_set = 0;
+	}
+	if (!empty($msg)){
+?>
 				<span style="color: red;">
 					<?php echo $msg;?>
 				</span>
-				<?php
-			}
-			?>
+<?php
+	}
+?>
 			<h3>
 				<?php print _("Forward for account");?>
 				<span style="color: red;">
-					<?php echo $username;?>
+					<?php echo $_GET['username'];?>
 				</span>
 			</h3>
-			<form action="<?php printf ('%s?domain=%s&amp;username=%s&amp;alias=%s&amp;action=%s', $_SERVER['PHP_SELF'], htmlspecialchars($_GET['domain']), htmlspecialchars($_GET['username']), htmlspecialchars($_GET['alias']), htmlspecialchars($_GET['action'])); ?>" method="post">
-
+			<form action="index.php" method="get">
+				<input type="hidden" name="action" value="forwardaccount">
+				<input type="hidden" name="confirmed" value="true">
+				<input type="hidden" name="domain" value="<?php echo $_GET['domain'];?>">
+				<input type="hidden" name="username" value="<?php echo $_GET['username'];?>">
 				<table>
 					<tr>
 						<td>
@@ -107,7 +95,7 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 									// If a mail is to be kept on the server,
 									// the $row[dest] also contains the $username
 									// -> filter it out
-									if (strtolower($forward) != strtolower($username)){
+									if (strtolower($forward) != strtolower($_GET['username'])){
 										$forwards[] = htmlspecialchars(trim($forward));
 									} else {
 									       $keep = ' checked ';
@@ -120,7 +108,7 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 							<br>
 							<input type="checkbox"
 							name="metoo" 
-							<?php echo (isset($keep))?($keep):('');?>
+							<?php echo (!empty($keep))?($keep):('');?>
 							>
 							<?php print _("Keep a copy in the user's mailbox");?>
 							<br>
@@ -152,15 +140,16 @@ if ($ref!=$_SERVER['SCRIPT_FILENAME']){
 				value="<?php echo _("Cancel");?>"
 				>
 			</form>
-			<?php
-		} else {
-			?>
+<?php
+} else {
+?>
 			<h3>
-				<?php echo $err_msg;?>
+				<?php print $err_msg;?>
 			</h3>
-			<?php
-		}
-		?>
+			<a href="index.php?action=accounts&domain=<?php echo $_GET['domain'];?>"><?php print _("Back");?></a>
+<?php
+}
+?>
 	</td>
 </tr>
 
