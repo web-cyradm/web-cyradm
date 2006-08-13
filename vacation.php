01189569cac5c791c946f26ac6fc1b052216bd77
lukasz <lukasz@1b53a13e-0819-0410-8fc2-cf376fe68ea6>
2006-08-13 13:56:41 +0000

    Added better UTF-8 support. Added separate input field for subject of the message. Moved into table. Added escaping of " and \ chars.

--- vacation.php
+++ vacation.php
@@ -17,7 +17,6 @@ if ($authorized){
 				include_once WC_BASE . '/lib/sieve-php.lib.php';
 				include_once WC_BASE . '/lib/sieve_strs.php';
 				$daemon = new sieve($CYRUS['HOST'],"2000", $CYRUS['ADMIN'], $CYRUS['PASS'], $_GET['username']);
-
 				switch ($_GET['mode']) {
 				case 'set':
 					if ($daemon->sieve_login()){
@@ -28,11 +27,13 @@ if ($authorized){
 						if (preg_match ("/subject.*\n(.*)$/iUs", $mess3, $matches)){
 							// remove 'subject:' and trailing space.
 							preg_match ("/subject.*(\w.*)\s*\r?\n/iU",$matches[0],$matches2);
-							$subject = $matches2[1];
-							$text = $matches[1];
+							// adding extra slashes because
+							// sieve_sendscript() calls stripslaches()
+							$subject = addslashes($matches2[1]);
+							$text = addslashes($matches[1]);
 						} else {
-							$subject = 'On vacation messages';
-							$text = $mess2;
+							$subject = addslashes($_GET['subject']);
+							$text = addslashes($mess2);
 						}
 						// remove leading lines.
 						if (preg_match ("/^\s*\n*(.*)$/s", $text, $matches)){
@@ -48,6 +49,7 @@ if ($authorized){
 							$forwards_script ='';
 						}
 						$script = $vacation_script.$forwards_script;
+						$script = iconv($charset,"UTF-8",$script);
 						if ($daemon->sieve_sendscript('sieve', $script)){
 							if ($daemon->sieve_setactivescript('sieve')){
 								$_msg = _("Vacation notice set");
@@ -139,19 +141,26 @@ if ($authorized){
 					<input type="hidden" name="domain" value="<?php print $_GET['domain']; ?>"> 
 					<input type="hidden" name="alias" value="<?php print $_GET['alias']; ?>"> 
 					<input type="hidden" name="username" value="<?php echo $_GET['username'];?>">
-
-					<input class="menu" type="radio"
-					name="mode" value="set" checked><?php
-					print _("Set vacation notice");?><br>
-
-					<input class="menu" type="radio"
-					name="mode" value="unset"><?php
-					print _("Remove vacation notice");?><br>
-
-					<?php
+				
+					<table>
+						<tr>
+							<td>
+								<input class="menu" type="radio"
+								name="mode" value="set" checked><?php
+								print _("Set vacation notice");?>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<input class="menu" type="radio"
+								name="mode" value="unset"><?php
+								print _("Remove vacation notice");?>
+							</td>
+						</tr>
+<?php
 					if ($daemon->sieve_login()){
 						$sieve_str = new sieve_strs;
-						$old_script = utf8_decode($sieve_str->get_old_script($daemon));
+						$old_script = iconv('UTF-8',$charset,$sieve_str->get_old_script($daemon));
 						if (preg_match("/(require \".*)(redirect \"|$)/Uis", $old_script, $matches)){
 							if (preg_match("/subject \".*/is",$matches[1],$matches2)){
 								$vacation_script = $matches2[0];
@@ -164,29 +173,41 @@ if ($authorized){
 					} else {
 						$vacation_script = "";
 					}
-					
 					preg_match ("/(subject \".*)\" \"(.*$)/is", $vacation_script, $matches);
 					if (count($matches) >= 2){
-						$vacation_script = $matches[1]."\n\n".$matches[2];
-						$vacation_script2 = preg_replace("/subject \"/", "Subject: ", $vacation_script);
-						$vacation_script = preg_replace("/\";$/", "", $vacation_script2);
+						$subject = stripslashes(preg_replace("/subject \"/", "", $matches[1]));
+						$vacation_script = $matches[2];
+						$vacation_script = stripslashes(preg_replace("/\";$/", "", $vacation_script));
 					} else {
+						$subject = '';
 						$vacation_script = '';
 					}
-					?>
-					<textarea name="vacation_text"
-					rows="6" cols="55"><?php
-					print $vacation_script;
-					?></textarea><br>
+?>
+						<tr>
+							<td>
+								<?php print _("Subject"); ?>:<br>
+								<input type="text" name="subject"
+								size="60" value="<?php echo htmlspecialchars($subject); ?>">
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<?php print _("Body"); ?>:<br>
+								<textarea name="vacation_text"
+								rows="6" cols="55"><?php
+								print $vacation_script;?></textarea>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<input class="button" type="submit"
+								value="<?php print _("Submit");?>"> 
 
-					<input class="button"
-					type="submit"
-					value="<?php print _("Submit");?>"> 
-
-					<input class="button" name="cancel"
-					type="submit"
-					value="<?php print _("Cancel");?>"> 
-
+								<input class="button" name="cancel"
+								type="submit" value="<?php print _("Cancel");?>">
+							</td>
+						</tr>
+					</table>
 				</form>
 <?php
 			}
